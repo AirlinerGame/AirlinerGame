510d10412bfbde1b86d8e398de922e061cffaf87
Patrick Trautmann <patrick.trautmann@gmail.com>
2015-04-01 22:35:02 +0200

    Libs included

--- compile.bat
+++ compile.bat
@@ -2,3 +2,4 @@ set compPath=
 for /D %%D in (%SYSTEMROOT%\Microsoft.NET\Framework\v4*) do set msbuild.exe=%%D\MSBuild.exe
 
 %msbuild.exe% AirPlaner.sln /property:Configuration=Release
+xcopy /s Libs/SDL.dll AirPlaner/bin/WindowsGL/Release/SDL.dll
