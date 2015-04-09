131b58d4cbb325596038315a2196efe63dc1a121
Patrick Trautmann <patrick.trautmann@gmail.com>
2015-04-09 13:01:16 +0200

    Missing File

--- AirPlaner/Game/Screen/CreateGameScreen.cs
+++ AirPlaner/Game/Screen/CreateGameScreen.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
 using AirPlaner.IO.Settings;
 using AirPlaner.Screen;
 using Microsoft.Xna.Framework;
@@ -17,6 +19,8 @@ namespace AirPlaner.Game.Screen
         public TextBox Firstname { get; set; }
         public TextBox Lastname { get; set; }
         public TextBox AirlineName { get; set; }
+
+        public ImageBox UserImageBox { get; set; }
 
         public Texture2D ProfilePicture { get; set; }
         public Texture2D AirlinePicture { get; set; }
@@ -60,7 +64,12 @@ namespace AirPlaner.Game.Screen
             {
                 if (!string.IsNullOrEmpty(ofDialog.SelectedFile))
                 {
-                    ProfilePicture = _content.Load<Texture2D>(ofDialog.SelectedFile);
+                    using (FileStream stream = new FileStream(ofDialog.SelectedFile, FileMode.Open, FileAccess.Read))
+                    {
+                        ProfilePicture = Texture2D.FromStream(ScreenManager.GraphicsDevice, stream);
+                    }
+                    UserImageBox.Image = ProfilePicture;
+                    UserImageBox.Refresh();
                 }
             };
         }
@@ -131,15 +140,17 @@ namespace AirPlaner.Game.Screen
 
         private Button _okayButton;
         private readonly TextBox _selectionBox;
+        public string CurrentDirectory { get; set; }
 
         public string SelectedFile { get; set; }
 
         public OpenFileDialog(ScreenManager screenManager) : base(screenManager.InternalGame.GuiManager)
         {
             ScreenManager = screenManager;
+            CurrentDirectory = Directory.GetCurrentDirectory();
 
             Width = 500;
-            Height = 260;
+            Height = 300;
             CloseButtonVisible = false;
             IconVisible = false;
             Resizable = false;
@@ -148,13 +159,25 @@ namespace AirPlaner.Game.Screen
             var horizontalWidth = Width - 50;
 
             var selectLabel = new Label(Manager);
-            selectLabel.Text = "Dateiname:";
+            selectLabel.Text = strings.txtFilename;
             selectLabel.Width = horizontalWidth / 3;
             selectLabel.Left = 10;
-            selectLabel.Top = Height - 140;
+            selectLabel.Top = Height - 100;
             selectLabel.Alignment = Alignment.MiddleRight;
             selectLabel.TextColor = Color.White;
 
+            ListBox = new ListBox(Manager);
+            ListBox.TextColor = Color.White;
+            ListBox.Width = OriginWidth - 30;
+            ListBox.Top = 10;
+            ListBox.Left = OriginWidth / 2 - ListBox.Width / 2 - 5;
+            ListBox.Height = selectLabel.Top - 20;
+
+            //Build Initial Items
+            RenderFileTree();
+
+            ListBox.DoubleClick += listBox_DoubleClick;
+            
             _selectionBox = new TextBox(screenManager.InternalGame.GuiManager);
             _selectionBox.Left = selectLabel.Left + selectLabel.Width + 10;
             _selectionBox.Top = selectLabel.Top;
@@ -170,9 +193,91 @@ namespace AirPlaner.Game.Screen
                 Close();
             };
 
+            Add(ListBox);
             Add(_okayButton);
             Add(selectLabel);
             Add(_selectionBox);
+        }
+
+        private void RenderFileTree()
+        {
+            ListBox.Items.Clear();
+
+            ListBox.Items.Add("..");
+
+            foreach (var dir in Directory.GetDirectories(CurrentDirectory))
+            {
+                string[] directories = dir.Split(Path.DirectorySeparatorChar);
+                ListBox.Items.Add(directories[directories.Length - 1] + "/");
+            }
+
+            foreach (var file in Directory.GetFiles(CurrentDirectory))
+            {
+                string[] directories = file.Split(Path.DirectorySeparatorChar);
+                ListBox.Items.Add(directories[directories.Length - 1]);
+            }
+            
+        }
+
+        public ListBox ListBox { get; set; }
+
+        private void listBox_DoubleClick(object sender, EventArgs e)
+        {
+            var selectedItem = ListBox.Items[ListBox.ItemIndex] as string;
+            if (selectedItem != null)
+            {
+                if (selectedItem.Equals(".."))
+                {
+                    //Move Directory up
+                    string[] directories = CurrentDirectory.Split(Path.DirectorySeparatorChar);
+                    if (directories.Length > 1)
+                    {
+                        CurrentDirectory = MoveDown(directories);
+                    }
+                }
+                else
+                {
+                    if (selectedItem.EndsWith("/"))
+                    {
+                        CurrentDirectory = MoveTo(selectedItem);
+                    }
+                    else
+                    {
+                        //File selected!
+                        SelectedFile = PathSelection(selectedItem);
+                        Close();
+                    }
+                }
+
+                RenderFileTree();
+            }
+        }
+
+        private string PathSelection(string selectedItem)
+        {
+            return CurrentDirectory + Path.DirectorySeparatorChar + selectedItem;
+        }
+
+        private string MoveTo(string selectedDir)
+        {
+            return CurrentDirectory + Path.DirectorySeparatorChar + selectedDir.Substring(0, selectedDir.Length - 1);
+        }
+
+        private string MoveDown(string[] directories)
+        {
+            string newPath = "";
+            for (int i = 0; i < directories.Length - 1; i++)
+            {
+                if (i == directories.Length - 2)
+                {
+                    newPath = newPath + directories[i];
+                }
+                else
+                {
+                    newPath = newPath + directories[i] + Path.DirectorySeparatorChar;                    
+                }
+            }
+            return newPath;
         }
 
         public override void Show()
